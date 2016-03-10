fb89af6ac65616ac080955d8d50ad89aece51f1c
Tomas Aparicio <tomas@aparicio.me>
2016-03-10 13:03:45 +0100

    feat(examples): add custom retry strategy example

---
+++ _examples/retry_strategy/retry.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/eapache/go-resiliency.v1/retrier"
+	"gopkg.in/h2non/gentleman-consul.v0"
+	"gopkg.in/h2non/gentleman-mock.v0"
+	"gopkg.in/h2non/gentleman.v0"
+	"gopkg.in/h2non/gock.v0"
+	"time"
+)
+
+const consulValidResponse = `
+[
+  {
+    "Node":"consul-client-nyc3-1",
+    "Address":"127.0.0.1",
+    "ServiceID":"web",
+    "ServiceName":"web",
+    "ServiceTags":[],
+    "ServiceAddress":"",
+    "ServicePort":80,
+    "ServiceEnableTagOverride":false,
+    "CreateIndex":17,
+    "ModifyIndex":17
+  }
+]`
+
+func main() {
+	defer gock.Off()
+
+	// Mock consul server
+	gock.New("http://demo.consul.io").
+		Get("/v1/catalog/service/web").
+		Reply(200).
+		Type("json").
+		BodyString(consulValidResponse)
+
+	// Configure failure responses
+	gock.New("http://127.0.0.1:80").
+		Get("/").
+		Times(10).
+		Reply(503)
+
+	// Final valid response
+	gock.New("http://127.0.0.1:80").
+		Get("/").
+		Reply(200).
+		SetHeader("Server", "gock").
+		BodyString("hello world")
+
+	// Create a new client
+	cli := gentleman.New()
+
+	// Configure Consul plugin
+	config := consul.NewConfig("demo.consul.io", "web")
+
+	// Use a custom retrier strategy with max 10 retry attempts
+	config.Retrier = retrier.New(retrier.ConstantBackoff(10, time.Duration(25*time.Millisecond)), nil)
+
+	// Intercept HTTP transport via gock to simulate the failures
+	gock.InterceptClient(config.Client.HttpClient)
+	cli.Use(mock.Plugin)
+
+	// Register Consul's plugin at client level
+	cli.Use(consul.New(config))
+
+	// Create a new request based on the current client
+	req := cli.Request()
+
+	// Set a new header field
+	req.SetHeader("Client", "gentleman")
+
+	// Perform the request
+	res, err := req.Send()
+	if err != nil {
+		fmt.Printf("Request error: %s\n", err)
+		return
+	}
+	if !res.Ok {
+		fmt.Printf("Invalid server response: %d\n", res.StatusCode)
+		return
+	}
+
+	// Reads the whole body and returns it as string
+	fmt.Printf("Server URL: %s\n", res.RawRequest.URL.String())
+	fmt.Printf("Response status: %d\n", res.StatusCode)
+	fmt.Printf("Server header: %s\n", res.Header.Get("Server"))
+}
--- consul_test.go
+++ consul_test.go
@@ -2,10 +2,12 @@ package consul
 
 import (
 	"github.com/nbio/st"
+	"gopkg.in/eapache/go-resiliency.v1/retrier"
 	"gopkg.in/h2non/gentleman-mock.v0"
 	"gopkg.in/h2non/gentleman.v0"
 	"gopkg.in/h2non/gock.v0"
 	"testing"
+	"time"
 )
 
 const consulValidResponse = `
@@ -68,6 +70,41 @@ func TestConsulRetry(t *testing.T) {
 	gock.New("http://127.0.0.1:80").
 		Get("/").
 		Times(3).
+		Reply(503)
+
+	gock.New("http://127.0.0.1:80").
+		Get("/").
+		Reply(200).
+		BodyString("hello world")
+
+	cli := gentleman.New()
+	cli.Use(mock.Plugin)
+	cli.Use(consul)
+
+	res, err := cli.Request().Send()
+	st.Expect(t, err, nil)
+	st.Expect(t, res.StatusCode, 200)
+	st.Expect(t, res.String(), "hello world")
+	st.Expect(t, gock.IsPending(), false)
+}
+
+func TestConsulRetryCustomStrategy(t *testing.T) {
+	defer gock.Off()
+
+	config := NewConfig("demo.consul.io", "web")
+	config.Retrier = retrier.New(retrier.ConstantBackoff(10, time.Duration(25*time.Millisecond)), nil)
+	consul := New(config)
+	gock.InterceptClient(config.Client.HttpClient)
+
+	gock.New("http://demo.consul.io").
+		Get("/v1/catalog/service/web").
+		Reply(200).
+		Type("json").
+		BodyString(consulValidResponse)
+
+	gock.New("http://127.0.0.1:80").
+		Get("/").
+		Times(10).
 		Reply(503)
 
 	gock.New("http://127.0.0.1:80").
